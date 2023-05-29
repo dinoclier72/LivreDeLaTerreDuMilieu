e49c53a872e727205351ecd21508bab05d75c7c0
Dino <72475325+dinoclier72@users.noreply.github.com>
2023-05-29 12:32:35 +0200

    debut de l'interface

--- app/src/main/java/com/tpgate/livredesmercenaires/MainActivity.kt
+++ app/src/main/java/com/tpgate/livredesmercenaires/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.tpgate.livredesmercenaires
 
+import android.app.Activity
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.transition.Transition
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -16,17 +13,17 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.disklrucache.DiskLruCache
-import com.bumptech.glide.request.target.SimpleTarget
+import android.view.View
+import android.widget.ImageButton
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavController
 import com.tpgate.livredesmercenaires.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object{
         lateinit var context : Context
         val handler = Handler(Looper.getMainLooper())
-
     }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -40,39 +37,56 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        val navController : NavController = findNavController(R.id.nav_host_fragment_content_main)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val characterButton: ImageButton = findViewById(R.id.characterButton)
+        val mapButton: ImageButton = findViewById(R.id.mapButton)
+        val mainMenuButton: ImageButton = findViewById(R.id.mainMenuButton)
+        val characterGuesserButton: ImageButton = findViewById(R.id.guessCharacterButton)
+        val quoteGuesserButton: ImageButton = findViewById(R.id.guessQuoteButton)
 
-        binding.fab.setOnClickListener { view ->
+        characterButton.setOnClickListener{
             navController.navigate(R.id.characterFragment)
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        mainMenuButton.setOnClickListener {
+            navController.navigate(R.id.FirstFragment)
         }
+
+//        setSupportActionBar(binding.toolbar)
+//
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+//        //configure the system bar to hide
+//        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+//        // Configure the behavior of the hidden system bars.
+//        windowInsetsController.systemBarsBehavior =  WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
 }
